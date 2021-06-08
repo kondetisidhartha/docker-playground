03ea1e85c1dac86cc62f09126c39d189e1cb08a4
kondetisidhartha <kondetisidhartha@gmail.com>
2021-06-09 01:00:58 +0200

    Volumes & Bind Mounts
    Working project able to detect changes in source code using bind mount and nodemon.

--- section-03/data-volumes-01/server.js
+++ section-03/data-volumes-01/server.js
@@ -31,6 +31,8 @@ app.post('/create', async (req, res) => {
   const tempFilePath = path.join(__dirname, 'temp', adjTitle + '.txt');
   const finalFilePath = path.join(__dirname, 'feedback', adjTitle + '.txt');
 
+  console.log("this is test line after adding nodemon!")
+
   await fs.writeFile(tempFilePath, content);
   exists(finalFilePath, async (exists) => {
     if (exists) {
